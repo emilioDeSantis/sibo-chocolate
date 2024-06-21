79545a86f5ab29392aa5967ce215699dc0cf6633
emilio <emilio5139@gmail.com>
2024-06-21 18:00:39 -0400

    first commit

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -4,17 +4,32 @@ import { CategoriesProvider } from "@/components/Providers/CategoriesProvider";
 import { ColorProvider } from "@/components/Providers/ColorProvider";
 import { LocationProvider } from "@/components/Providers/LocationProvider";
 import "@/styles/globals.css";
+import Head from 'next/head';
+import Script from 'next/script';
 
 export default function App({ Component, pageProps }) {
     return (
-        <ColorProvider>
-            <BagProvider>
-                <CategoriesProvider>
-                    <LocationProvider>
-                        <Component {...pageProps} />
-                    </LocationProvider>
-                </CategoriesProvider>
-            </BagProvider>
-        </ColorProvider>
+        <>
+            <Head>
+                <Script async src="https://www.googletagmanager.com/gtag/js?id=G-KSLZDSQYNX" />
+                <Script>
+                    {`
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', 'G-KSLZDSQYNX');
+                    `}
+                </Script>
+            </Head>
+            <ColorProvider>
+                <BagProvider>
+                    <CategoriesProvider>
+                        <LocationProvider>
+                            <Component {...pageProps} />
+                        </LocationProvider>
+                    </CategoriesProvider>
+                </BagProvider>
+            </ColorProvider>
+        </>
     );
 }
